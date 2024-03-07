96d829de019fa6ebcc5050f90dedd54392b55671
bhavyaverma08 <verma100bhavya@gmail.com>
2024-03-07 15:13:52 +0530

    ADD layout components & pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
 import { HomeComponent } from './pages/home/home.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
+import { ViewTodoComponent } from './pages/view-todo/view-todo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, title: 'Home of Todo Manager' },
+  { path: 'home', component: HomeComponent, title: 'Home - Todo Manager' },
+  { path: 'add', component: AddTodoComponent, title: 'Add - Todo Manager' },
+  { path: 'view', component: ViewTodoComponent, title: 'View - Todo Manager' },
+  {
+    path: 'update',
+    component: UpdateTodoComponent,
+    title: 'Update - Todo Manager',
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { ViewTodoComponent } from './pages/view-todo/view-todo.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AddTodoComponent,
+    ViewTodoComponent,
+    UpdateTodoComponent
   ],
   imports: [
     BrowserModule,
